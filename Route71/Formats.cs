5f9f65e7b5b7088277222e1fc6413d6b88bea0d7
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2023-06-13 20:55:26 +0200

    Add project files.

---
+++ Route71/Formats.cs
@@ -0,0 +1,19 @@
+﻿using ImageMagick;
+
+namespace Route71;
+
+internal class Formats : Casasoft.CCDV.Formats
+{
+    public Formats() : base()
+    {
+    }
+
+    public Formats(int dpi) : base(dpi)
+    {
+    }
+
+    public MagickGeometry Poster_v => new(ToPixels(302), ToPixels(452));
+    public MagickGeometry HalfPoster_o => new(Poster_v.Width, Poster_v.Height / 2 - ToPixels(4));
+    private int vsize => ToPixels(190);
+    public MagickGeometry InternalImage => new(vsize / 3 * 4, vsize);
+}
---
+++ Route71/Images.cs
@@ -0,0 +1,53 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route71;
+
+internal class Images : Casasoft.CCDV.Images
+{
+    
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="dpi">Resolution of images</param>
+    public Images(int dpi) : base(dpi)
+    {
+        fmt = new Formats(dpi);
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <remarks>
+    /// The resolution is set to the default 300 DPI
+    /// </remarks>
+    public Images() : this(300)
+    {
+
+    }
+
+    /// <summary>
+    /// Construcot
+    /// </summary>
+    /// <param name="f">
+    /// Instance of <see cref="Casasoft.CCDV.Formats"/> class used to set the
+    /// resolution of the images
+    /// </param>
+    public Images(Casasoft.CCDV.IFormats f) : base(f)
+    {
+        fmt = new Formats(f.DPI);
+    }
+
+    public MagickImage Poster_v(MagickColor c) => new(c, ((Formats)fmt).Poster_v.Width, ((Formats)fmt).Poster_v.Height);
+    public MagickImage HalfPoster_o(MagickColor c) => new(c, ((Formats)fmt).HalfPoster_o.Width, ((Formats)fmt).HalfPoster_o.Height);
+    public MagickImage Poster_v() => Poster_v(MagickColors.White);
+    public MagickImage HalfPoster_o() => HalfPoster_o(MagickColors.White);
+    public MagickImage InternalImage(MagickColor c) => new(c, ((Formats)fmt).InternalImage.Width, ((Formats)fmt).InternalImage.Height);
+    public MagickImage InternalImage() => InternalImage(MagickColors.White);
+
+
+}
